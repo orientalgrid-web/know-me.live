b5b9b1e608c54a8705ec4ff6ffd23b14a1ed1b04
Uchechukwu Eke <uchechukwu.eke.orientalgrid@gmail.com>
2026-09-18 00:25:50 +0100

    Restore original full React application code

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,6 @@
-import React from 'react'
-
-export default function App() {
-  return (
-    <div style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
-      <h1>KnowMe Social Network</h1>
-      <p>App deployed successfully!</p>
-    </div>
-  )
+"scripts": {
+  "test": "echo \"Error: no test specified\" && exit 1"
+}"scripts": {
+  "start": "node index.js",
+  "dev": "nodemon index.js"
 }
